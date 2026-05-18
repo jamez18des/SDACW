71ffe49b70c57dfac2eb0e9f0b8192bae7266cf1
jamesdeslandes <james.18des@gmail.com>
2026-05-18 03:40:14 +0100

    4 player large board

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,18 @@
+import java.util.List;
+
 public class Game {
 
-    private final Red red;
-    private final Blue blue;
+    private final List<BasePlayer> players;
     private final Dice dice;
     private final Board board;
     private final HitRuleStrategy hitRule;
     private final TeleportRuleStrategy teleportRule;
 
-    public Game(Dice dice, EndRuleStrategy endRule,
+    public Game(List<BasePlayer> players, Dice dice, Board board,
                 HitRuleStrategy hitRule, TeleportRuleStrategy teleportRule) {
-        this.red = new Red(endRule);
-        this.blue = new Blue(endRule);
+        this.players = players;
         this.dice = dice;
-        this.board = new Board(5, 5);
+        this.board = board;
         this.hitRule = hitRule;
         this.teleportRule = teleportRule;
     }
@@ -20,113 +20,86 @@ public class Game {
     public void play() {
         System.out.println("Board: rows=" + board.getRows()
                 + " columns=" + board.getColumns());
-        System.out.println("Players: 2");
+        System.out.println("Players: " + players.size());
         System.out.println();
-        System.out.println("Red " + getTrack(new int[]{
-                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-                16,17,18,19,20,21,22,23,24,25}));
-        System.out.println("Blue " + getTrack(new int[]{
-                25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,
-                10,9,8,7,6,5,4,3,2,1}));
+        for (BasePlayer p : players) {
+            System.out.println(p.getName() + " " + p.getTrackDescription());
+        }
         System.out.println();
         System.out.println("Game State: Ready -> InPlay");
         System.out.println();
 
-        int redTurns = 0;
-        int blueTurns = 0;
+        int[] turnCounts = new int[players.size()];
         int totalTurns = 0;
 
+        outer:
         while (true) {
+            for (int i = 0; i < players.size(); i++) {
+                BasePlayer current = players.get(i);
+                turnCounts[i]++;
+                totalTurns++;
 
-            // Red's turn
-            redTurns++;
-            totalTurns++;
-            int redRoll = dice.roll();
-            String redFrom = formatPosition(red.getPosition(), red.getHomePosition(), red.getEndPosition());
-            int redPeek = red.peekPosition(redRoll);
-            System.out.println("Red turn " + redTurns + " rolls " + redRoll);
-            System.out.println("Red moves from " + redFrom + " to "
-                    + formatPosition(redPeek, red.getHomePosition(), red.getEndPosition()));
+                int roll = dice.roll();
+                String from = formatPosition(current.getPosition(),
+                        current.getHomePosition(), current.getEndPosition());
+                int peek = current.peekPosition(roll);
 
-            if (redPeek == blue.getPosition()) {
-                System.out.println("Red hit Blue at position Position " + redPeek);
-                if (hitRule.allowMove(redPeek, blue.getPosition())) {
-                    red.advance(redRoll);
+                System.out.println(current.getName() + " turn "
+                        + turnCounts[i] + " rolls " + roll);
+                System.out.println(current.getName() + " moves from " + from
+                        + " to " + formatPosition(peek,
+                        current.getHomePosition(), current.getEndPosition()));
+
+                BasePlayer hitTarget = findHit(current, peek);
+
+                if (hitTarget != null && peek != current.getEndPosition()) {
+                    System.out.println(current.getName() + " hit "
+                            + hitTarget.getName()
+                            + " at position Position " + peek);
+                    if (hitRule.allowMove(peek, hitTarget.getPosition())) {
+                        current.advance(roll);
+                    } else {
+                        System.out.println(current.getName() + " moves from "
+                                + peek + " to " + from);
+                    }
                 } else {
-                    System.out.println("Red moves from " + redPeek + " to " + redFrom);
+                    current.advance(roll);
                 }
-            } else {
-                red.advance(redRoll);
-            }
 
-            if (!red.isAtEnd()) {
-                int teleported = teleportRule.applyRule(red.getPosition());
-                if (teleported != red.getPosition()) {
-                    System.out.println("Red is teleported. Red moves from "
-                            + red.getPosition() + " to " + teleported);
-                    // set red position - add setPosition to Red too
+                if (!current.isAtEnd()) {
+                    int teleported = teleportRule.applyRule(current.getPosition());
+                    if (teleported != current.getPosition()) {
+                        System.out.println(current.getName()
+                                + " is teleported. " + current.getName()
+                                + " moves from " + current.getPosition()
+                                + " to " + teleported);
+                        current.setPosition(teleported);
+                    }
                 }
-            }
 
-            if (red.isAtEnd()) {
-                System.out.println("Red wins in " + redTurns
-                        + " turns. Total turns: " + totalTurns + ".");
-                System.out.println("Game State: InPlay -> GameOver");
-                return;
-            }
-
-            // Blue's turn
-            blueTurns++;
-            totalTurns++;
-            int blueRoll = dice.roll();
-            String blueFrom = formatPosition(blue.getPosition(), blue.getHomePosition(), blue.getEndPosition());
-            int bluePeek = blue.peekPosition(blueRoll);
-            System.out.println("Blue turn " + blueTurns + " rolls " + blueRoll);
-            System.out.println("Blue moves from " + blueFrom + " to "
-                    + formatPosition(bluePeek, blue.getHomePosition(), blue.getEndPosition()));
-
-            if (bluePeek == red.getPosition()) {
-                System.out.println("Blue hit Red at position Position " + bluePeek);
-                if (hitRule.allowMove(bluePeek, red.getPosition())) {
-                    blue.advance(blueRoll);
-                } else {
-                    System.out.println("Blue moves from " + bluePeek + " to " + blueFrom);
+                if (current.isAtEnd()) {
+                    System.out.println(current.getName() + " wins in "
+                            + turnCounts[i] + " turns. Total turns: "
+                            + totalTurns + ".");
+                    System.out.println("Game State: InPlay -> GameOver");
+                    break outer;
                 }
-            } else {
-                blue.advance(blueRoll);
-            }
-
-            if (!blue.isAtEnd()) {
-                int teleported = teleportRule.applyRule(blue.getPosition());
-                if (teleported != blue.getPosition()) {
-                    System.out.println("Blue is teleported. Blue moves from "
-                            + blue.getPosition() + " to " + teleported);
-                    blue.setPosition(teleported);
-                }
-            }
-
-            if (blue.isAtEnd()) {
-                System.out.println("Blue wins in " + blueTurns
-                        + " turns. Total turns: " + totalTurns + ".");
-                System.out.println("Game State: InPlay -> GameOver");
-                return;
             }
         }
+    }
+
+    private BasePlayer findHit(BasePlayer current, int peekPos) {
+        for (BasePlayer other : players) {
+            if (other != current && other.getPosition() == peekPos) {
+                return other;
+            }
+        }
+        return null;
     }
 
     private String formatPosition(int pos, int home, int end) {
         if (pos == home) return "Home (Position " + pos + ")";
         if (pos == end) return "End (Position " + pos + ")";
         return String.valueOf(pos);
-    }
-
-    private String getTrack(int[] positions) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Home (Position ").append(positions[0]).append(")");
-        for (int i = 1; i < positions.length - 1; i++) {
-            sb.append(", ").append(positions[i]);
-        }
-        sb.append(", End (Position ").append(positions[positions.length - 1]).append(")");
-        return sb.toString();
     }
 }
